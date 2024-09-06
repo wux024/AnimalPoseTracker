b0848d33038e431db24ccf8df8ba8bca83f19da9
Adam Wiveion <wux024@nenu.edu.cn>
2024-09-06 16:50:48 +0800

    update

---
+++ animalposetracker/cores/tasks.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+File Name: tasks.py.py
+Author: wux024
+Email: wux024@nenu.edu.cn
+Created On: 2024/9/6
+Last Modified: 2024/9/6
+Version: 1.0
+
+Overview:
+    Provide a concise summary of the file's functionality, objectives, or primary logic implemented.
+    
+Notes:
+    - Modifications should be documented in the "Revision History" section beneath this.
+    - Ensure compliance with project coding standards.
+
+Revision History:
+    - [2024/9/6] wux024: Initial file creation
+"""
